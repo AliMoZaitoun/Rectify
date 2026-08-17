0d2bee3a23ffb08870445da84429186484cd9471
AliMoZaitoun <alimozaitoun@gmail.com>
2026-08-17 16:23:22 +0300

    Notification

--- app/Services/Complaint/ComplaintLifecycleService.php
+++ app/Services/Complaint/ComplaintLifecycleService.php
@@ -93,9 +93,9 @@ class ComplaintLifecycleService
             return $complaint->fresh(['histories', 'assignedTo', 'client.user']);
         });
 
-        if ($oldStatusValue !== $newStatusValue) {
-            ComplaintStatusUpdated::dispatch($updatedComplaint, $oldStatusValue);
-        }
+        ComplaintStatusUpdated::dispatch($updatedComplaint, $oldStatusValue);
+        // if ($oldStatusValue !== $newStatusValue) {
+        // }
 
         return $updatedComplaint;
     }
